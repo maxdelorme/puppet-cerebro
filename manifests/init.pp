e7a19a5bfd5389b7fc1fdcbd39f02af160307023
Brandon Wulf <mrwulf@gmail.com>
2017-08-21 16:01:30 -0700

    Adding the ability to pass multiple targets and do simple authentication

--- manifests/init.pp
+++ manifests/init.pp
@@ -20,25 +20,25 @@
 # Copyright 2017 Brandon Wulf
 #
 class cerebro (
-  $version      = '0.6.6',
-  $download_url = "https://github.com/lmenezes/cerebro/releases/download/v${version}/cerebro-${version}.tgz",
-  $install_path = '/opt/cerebro',
-  $user         = 'cerebro',
-  $group        = 'cerebro',
-  $service      = 'cerebro',
-  $target_url   = 'http://localhost:9200',
-  $target_name  = 'Local Elasticsearch',
+  $version       = '0.6.6',
+  $download_url  = "https://github.com/lmenezes/cerebro/releases/download/v${version}/cerebro-${version}.tgz",
+  $install_path  = '/opt/cerebro',
+  $user          = 'cerebro',
+  $group         = 'cerebro',
+  $service       = 'cerebro',
+  $targets       = [ {
+    host => 'http://localhost:9200',
+    name => 'Local Elasticsearch',
+  } ],
+  $auth_type     = undef,
+  $auth_settings = undef,
 ) {
   require '::archive'
 
-  if (!defined('::java')) {
-    class { '::java':
-      distribution          => 'jdk',
-      package               => 'jdk1.8.0_73',
-      java_alternative      => 'java',
-      java_alternative_path => '/usr/java/jdk1.8.0_73/jre/bin/java',
-      before                => Service[ $service ],
-    }
+  validate_array($targets)
+
+  if $auth_type {
+    validate_hash( $auth_settings )
   }
 
   group{ $group:
@@ -54,7 +54,7 @@ class cerebro (
     ensure => 'directory',
     owner  => $user,
     group  => $group,
-    mode   => '0555',
+    mode   => '0775',
   }
 
   -> archive{ "${install_path}/downloads/download-${version}.tgz":
@@ -81,7 +81,7 @@ class cerebro (
 
   -> file{ "/etc/systemd/system/${service}.service":
     ensure  => 'file',
-    mode    => '0550',
+    mode    => '0664',
     content => template("${module_name}/cerebro.systemd.erb"),
   }
 
@@ -89,15 +89,16 @@ class cerebro (
     ensure => 'directory',
     owner  => $user,
     group  => $group,
-    mode   => '0555',
+    mode   => '0775',
   }
 
   -> file{ '/etc/cerebro/application.conf':
     ensure  => 'file',
-    mode    => '0550',
+    mode    => '0664',
     owner   => $user,
     group   => $group,
     content => template("${module_name}/application.conf.erb"),
+    notify  => Service[ $service ],
   }
 
   -> file{ "${install_path}/current/conf/application.conf":
